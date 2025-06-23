8ed6102ce6f2c1badfca298ec422dc731853157f
goumarry <yoann.groumarre@etu.univ-grenoble-alpes.fr>
2025-06-23 19:53:30 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/Portfolio_Yoann_Gouarre_2025/',
   plugins: [vue()],
 })
